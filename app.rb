7f06fb5d6b53042afcaef88d2b5e75835a86837f
Virag Kormoczy <vkormoczy@gmail.com>
2022-09-28 17:13:26 +0300

    Add create_student and create_teacher methods

--- app.rb
+++ app.rb
@@ -3,9 +3,10 @@ require_relative 'person'
 require_relative 'rental'
 require_relative 'student'
 require_relative 'teacher'
+require_relative 'classroom'
 
 class App
-  attr_accessor :book_list
+  attr_accessor :book_list, :people
 
   def initialize
     @book_list = []
@@ -24,5 +25,20 @@ class App
 
   def list_all_books
     @book_list.empty? ? "The list is empty, add some books..." : @book_list.each { |book| puts book.title }
+  end
+
+  def create_student(age, classroom, name)
+    class_room = Classroom.new(classroom)
+    student = Student.new(age, class_room, name)
+    @people << student unless @people.include?(student)
+  end
+
+  def create_teacher(age, specialization, name)
+    teacher = Teacher.new(age, specialization, name)
+    @people << teacher unless @people.include?(teacher)
+  end
+
+  def list_all_people
+    @people.empty? ? "The list is empty, add some people..." : @people.each { |person| puts person.name }
   end
 end
