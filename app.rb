04dd031c2c277f9cce94bbf8a982237e6e48ed0d
Virag Kormoczy <vkormoczy@gmail.com>
2022-09-28 21:00:55 +0300

    Add create_rentals and list_all_rentals methods

--- app.rb
+++ app.rb
@@ -20,13 +20,51 @@ class App
     prompt_user
   end
 
-  def create_book(title, author)
+  def create_book
+    puts 'Enter the title of the book:'
+    title = gets.chomp
+    puts 'Enter the author of the book:'
+    author = gets.chomp
     book = Book.new(title, author)
+    puts "The book \'#{title}\' by #{author} is created successfully!"
     @book_list << book unless @book_list.include?(book)
   end
 
+  def create_rental
+    puts "Select a book from the following list by number:\n"
+    list_all_books
+    index_of_book = gets.chomp.to_i
+    puts "Select a person from the following list by number:\n"
+    list_all_people
+    index_of_person = gets.chomp.to_i
+    puts 'Enter a date: e.g 2022/09/28'
+    date = gets.chomp.to_i
+    create_rental(date, book_list[index_of_book], people[index_of_person])
+    rental = Rental.new(date, person, book)
+    puts 'Rental successfully created!'
+    @rentals << rental unless @rentals.include?(rental)
+  end
+
+  def list_all_rentals
+    puts "Rentals list:\n\n"
+    if @rentals.empty?
+      'The list is empty, add some rentals...'
+    else
+      @rentals.each_with_index do |rental, index|
+        puts "#{index}) Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}"
+      end
+    end
+  end
+
   def list_all_books
-    @book_list.empty? ? 'The list is empty, add some books...' : @book_list.each { |book| puts "Title: '#{book.title}', Author: #{book.author} " }
+    puts "Books list:\n\n"
+    if @book_list.empty?
+      'The list is empty, add some books...'
+    else
+      @book_list.each_with_index do |book, index|
+        puts "#{index}) Title: '#{book.title}', Author: #{book.author}"
+      end
+    end
   end
 
   def create_student(age, classroom, name)
@@ -42,15 +80,49 @@ class App
     @teachers << teacher unless @teachers.include?(teacher)
   end
 
+  def create_person
+    puts 'Which do you want to create? A student (press 1) or a teacher (press 2)?'
+    num = gets.chomp.to_i
+    case num
+    when 1
+      student_option
+    when 2
+      teacher_option
+    end
+  end
+
+  def student_option
+    puts 'Enter the age of the student:'
+    age = gets.chomp.to_i
+    puts "Enter the student's classroom (number):"
+    classroom = gets.chomp.to_i
+    puts "Enter the student's name:"
+    name = gets.chomp
+    create_student(age, classroom, name)
+    puts "The student named '#{name}' of age #{age} with the classroom number #{classroom} was created successfully!"
+  end
+
+  def teacher_option
+    puts 'Enter the age of the teacher:'
+    age = gets.chomp.to_i
+    puts "Enter the teacher's specialization:"
+    specialization = gets.chomp
+    puts "Enter the teacher's name:"
+    name = gets.chomp
+    create_teacher(age, specialization, name)
+    puts "The teacher named '#{name}' of age #{age} with the specialization #{specialization} was created successfully!"
+  end
+
   def list_all_people
+    puts "People's list:\n\n"
     if @people.empty?
       puts 'The list is empty, add some people...'
     else
-      @people.each do |person|
+      @people.each_with_index do |person, index|
         if person.instance_of?(Student)
-          puts "[Student] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+          puts "#{index}) [Student] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
         else
-          puts "[Teacher] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+          puts "#{index}) [Teacher] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
         end
       end
     end
