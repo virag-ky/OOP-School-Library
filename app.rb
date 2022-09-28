583912d6a6953ad7f52b92bdafd1eb8f20a421e5
Virag Kormoczy <vkormoczy@gmail.com>
2022-09-28 19:02:19 +0300

    Update books list and people's list

--- app.rb
+++ app.rb
@@ -11,6 +11,8 @@ class App
   def initialize
     @book_list = []
     @people = []
+    @students = []
+    @teachers = []
     @rentals = []
   end
 
@@ -24,21 +26,41 @@ class App
   end
 
   def list_all_books
-    @book_list.empty? ? "The list is empty, add some books..." : @book_list.each { |book| puts book.title }
+    @book_list.empty? ? 'The list is empty, add some books...' : @book_list.each { |book| puts "Title: '#{book.title}', Author: #{book.author} " }
   end
 
   def create_student(age, classroom, name)
     class_room = Classroom.new(classroom)
     student = Student.new(age, class_room, name)
     @people << student unless @people.include?(student)
+    @students << student unless @students.include?(student)
   end
 
   def create_teacher(age, specialization, name)
     teacher = Teacher.new(age, specialization, name)
     @people << teacher unless @people.include?(teacher)
+    @teachers << teacher unless @teachers.include?(teacher)
   end
 
   def list_all_people
-    @people.empty? ? "The list is empty, add some people..." : @people.each { |person| puts person.name }
+    if @people.empty?
+      puts 'The list is empty, add some people...'
+    else
+      @people.each do |person|
+        if person.instance_of?(Student)
+          puts "[Student] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+        else
+          puts "[Teacher] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+        end
+      end
+    end
+  end
+
+  def list_all_students
+    @students.empty? ? 'The list is empty, add some students...' : @students.each { |student| puts student.name }
+  end
+
+  def list_all_teachers
+    @teachers.empty? ? 'The list is empty, add some teachers...' : @teachers.each { |teacher| puts teacher.name }
   end
 end
